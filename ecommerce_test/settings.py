7448e1f537053aec097d7cf3c1d3d14d6f383e5c
Collins Lagat <cklagat@gmail.com>
2025-08-16 11:54:01 +0300

    feat: make more secure by dynamically setting the secret key and debug mode

--- ecommerce_test/settings.py
+++ ecommerce_test/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-aqp2df#fdz5z+_5@6@!)!vd^&@kbysph+=)5_(!f2q(r%s9s8u"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
