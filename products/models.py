f7692d3c9da779444b66d34995acfda703ea5fca
Collins Lagat <cklagat@gmail.com>
2025-08-19 17:04:54 +0300

    feat: add description and image to product and average price to category

--- products/models.py
+++ products/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=30)
+    description = models.TextField(default="")
+    image = models.ImageField(upload_to="products/images/", null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey("products.Category", on_delete=models.CASCADE)
 
@@ -19,6 +21,7 @@ class Category(models.Model):
         blank=True,
         related_name="children",
     )
+    average_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return self.name
