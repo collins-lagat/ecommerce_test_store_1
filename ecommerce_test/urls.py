465ff3d0ca5beb324be09fb92edde88e922e4af8
Collins Lagat <cklagat@gmail.com>
2025-08-19 17:03:55 +0300

    feat: setup static and media

--- ecommerce_test/urls.py
+++ ecommerce_test/urls.py
@@ -17,11 +17,17 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("social_django.urls", namespace="social")),
-    path("", views.index, name="index"),
-]
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("", include("social_django.urls", namespace="social")),
+        path("", views.index, name="index"),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
