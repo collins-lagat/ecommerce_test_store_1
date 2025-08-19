102b081ebaaa511b954e49245a6976daf2b3cb88
Collins Lagat <cklagat@gmail.com>
2025-08-19 17:09:24 +0300

    feat: update index to display products

--- ecommerce_test/views.py
+++ ecommerce_test/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from products.models import Category
+
 
 def index(request):
-    return render(request, "index.html")
-
+    categories = Category.objects.filter(parent=None)
+    return render(request, "index.html", {"categories": categories})
