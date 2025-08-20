fcfa371b2ec70d1309cc3f6dc01d0b7ed794e821
Collins Lagat <cklagat@gmail.com>
2025-08-20 21:11:09 +0300

    fix: using the wrong timezone object

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from django.utils.http import timezone
-from rest_framework import mixins, status, viewsets
+from django.utils import timezone
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
