12ffcfeb224292a16cedf3c1feecb62591715c63
Collins Lagat <cklagat@gmail.com>
2025-08-20 19:14:45 +0300

    feat: load items along with cart

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,13 +3,15 @@ from rest_framework import serializers
 from orders.models import Item, Order
 
 
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = "__all__"
-
-
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
+        fields = "__all__"
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    item_set = ItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
         fields = "__all__"
