7080de095a93968f516d9dd72d2b8060183e9b1b
Collins Lagat <cklagat@gmail.com>
2025-08-20 17:18:08 +0300

    feat: add apis for products and categories

--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from products import views
 
@@ -6,4 +6,5 @@ from products import views
 urlpatterns = [
     path("category/<int:category_id>/", views.category, name="category"),
     path("product/<int:product_id>/", views.product, name="product"),
+    path("api/", include(views.router.urls)),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import get_object_or_404, render
+from rest_framework import viewsets, routers, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from products.models import Category, Product
+from products.serializers import CategorySerializer, ProductSerializer
 
 
 def category(request, category_id):
@@ -14,3 +18,24 @@ def category(request, category_id):
 def product(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, "product-single.html", {"product": product})
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @action(detail=True, methods=["get"])
+    def average_price(self, request, pk=None):
+        category = self.get_object()
+        return Response({"average_price": category.average_price})
+
+
+class ProductViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+router = routers.DefaultRouter()
+
+router.register(r"category", CategoryViewSet)
+router.register(r"product", ProductViewSet)
